b5a25ff3f733b3d6ace27dfafaf6c0bb53d2ba42
Evgeniy Terin <web@derin.ru>
2022-01-24 23:20:49 +0300

    кастомизация бутсрапа

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-import _ from 'lodash';
+import 'bootstrap';
+import './scss/index.scss';
 
-function component() {
-    const element = document.createElement('div');
-
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
-}
-
-document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,45 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Adds CSS to the DOM by injecting a `<style>` tag
+                        loader: 'style-loader'
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader'
+                    },
+                    {
+                        // Run postcss actions
+                        loader: 'postcss-loader',
+                        options: {
+                            // `postcssOptions` is needed for postcss 8.x;
+                            // if you use postcss 7.x skip the key
+                            postcssOptions: {
+                                // postcss plugins, can be exported to postcss.config.js
+                                plugins: function () {
+                                    return [
+                                        require('autoprefixer')
+                                    ];
+                                }
+                            }
+                        }
+
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    }
 };
